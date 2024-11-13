853fe476746fe084b81038dbc46ba918ce9cd231
Aunim HASSAN <aunim.hassan@gmail.com>
2024-11-13 11:30:12 +0100

    Create customers.py

---
+++ course/customers.py
@@ -0,0 +1,47 @@
+"""
+Client 1
+Nom : Doe
+Prénom : John
+Age : 21
+Email : john.doe@xyz.com
+Hobbies : Karaté, Tennis
+
+Client 2
+Nom : Stewart
+Prénom : Jane
+Age : 26
+Email : [Email non renseigné]
+Hobbies : Danse, Peinture, Chant
+
+Client 3
+Nom : Tardieu
+Prénom : Olivier
+Age : 32
+Email : olivier.tardieu@xyz.com
+Hobbies : [Hobbies non renseignés]
+"""
+
+customers = [
+    {
+        "last_name": "Doe",
+        "first_name": "John", # trailing comma
+        "age": 21,
+        "email": "john.doe@xyz.com",
+        "hobbies": ["Karaté", "Tennis"]
+    },
+    {
+        "last_name": "Doe",
+        "first_name": "John", # trailing comma
+        "age": 21,
+        "email": "john.doe@xyz.com",
+        "hobbies": ["Natation", "Musique"]
+    },
+        {
+        "last_name": "Doe",
+        "first_name": "John", # trailing comma
+        "age": 21,
+        "email": "john.doe@xyz.com",
+        "hobbies": ["Natation", "Musique"]
+    }
+]
+
